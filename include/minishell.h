a69eb8cf8520249db8a0584691a2e193b494ccc5
nyj001012 <nyj001012@gmail.com>
2023-02-28 12:43:20 +0900

    new(minishell.h): token type 상세화, node type 추가, ASTnode 부모 노드 필드 추가

--- include/minishell.h
+++ include/minishell.h
@@ -1,10 +1,14 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+#include "../libft/libft.h"
+
 enum	e_token_type {
 	NORMAL = -1,
 	PIPE = '|',
+	DPIPE = "||",
 	AMPERSAND = '&',
+	DAMPERSAND = "&&",
 	QUOTE = '\'',
 	DQUOTE = '\"',
 	SEMICOLON = ';',
@@ -15,12 +19,27 @@ enum	e_token_type {
 	REDIRECT_OUT = '<',
 	DREDIRECT_IN = '<<',
 	DREDIRECT_OUT = '>>',
+	WILDCARD = '*',
+	PARENTHESIS_OPEN = '(',
+	PARENTHESIS_CLOSE = ')',
+};
+
+enum	e_node_type {
+	PIPE_NODE,
+	DPIPE_NODE,
+	BACKGROUND_NODE,
+	SEQUENCE_NODE,
+	REDIRECT_IN_NODE,
+	REDIRECT_OUT_NODE,
+	CMD_PATH_NODE,
+	ARGUMENT_NODE,
 };
 
 typedef struct s_ASTnode
 {
 	int					type;
 	char				*value;
+	struct s_ASTnode	*parent;
 	struct s_ASTnode	*left;
 	struct s_ASTnode	*right;
 }	t_ASTnode;
