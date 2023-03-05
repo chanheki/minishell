76e1c01154890ea9b0aebb948890835815f46ada
chanhihi <chanhihi55@gmail.com>
2023-03-05 17:36:50 +0900

    new(global): struct

--- include/minishell.h
+++ include/minishell.h
@@ -1,6 +1,19 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# define PROMPT "\033[0;32;3mJIP-Shell$\033[0m "
+
+# include "../lib/libft.h"
+# include "readline/readline.h"
+# include <readline/history.h>
+# include <term.h>
+# include <stdio.h>
+# include <sys/ioctl.h>
+
+/* custom includes */
+# include "execute.h"
+# include "utill.h"
+
 enum	e_token_type {
 	NORMAL = -1,
 	PIPE = '|',
@@ -12,9 +25,9 @@ enum	e_token_type {
 	TAB = '\t',
 	NEW_LINE = '\n',
 	REDIRECT_IN = '>',
-	REDIRECT_OUT = '<',
-	DREDIRECT_IN = '<<',
-	DREDIRECT_OUT = '>>',
+	REDIRECT_OUT = '<'
+	// DREDIRECT_IN = '<<',
+	// DREDIRECT_OUT = '>>',
 };
 
 typedef struct s_ASTnode
@@ -24,5 +37,12 @@ typedef struct s_ASTnode
 	struct s_ASTnode	*left;
 	struct s_ASTnode	*right;
 }	t_ASTnode;
+
+typedef struct s_global
+{
+	char			**envp;
+}	t_global;
+
+extern t_global	g_var;
 
 #endif
