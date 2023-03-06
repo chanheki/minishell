737092a304177bf8b5a74d2666b5485c0dda7223
nyj001012 <nyj001012@gmail.com>
2023-03-06 11:22:53 +0900

    update(tokenize): trimmed_line == NULL일 때 종료, token 내 value가 없을 시, trimmed_line을 복제 후 종료하도록 변경

--- parse/token/join_token_value.c
+++ parse/token/join_token_value.c
@@ -6,7 +6,14 @@ void	join_token_value(t_token **token, char *trimmed_line, int *i)
 	char	*temp;
 
 	if (!token || !*token)
-		token = create_new_token(ft_strdup(trimmed_line[*i]));
+		token = create_new_token(NULL, NULL);
+	else if (!trimmed_line || !trimmed_line[*i])
+		return ;
+	if (!(*token)->value)
+	{
+		(*token)->value = ft_strdup(&trimmed_line[*i]);
+		return ;
+	}
 	else
 	{
 		new_value = ft_strjoin(*token->value, &trimmed_line[*i]);
