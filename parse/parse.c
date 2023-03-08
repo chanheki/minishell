4f3b2ddac7741afb5390633694c1c8a96c70101d
yena <50291995+nyj001012@users.noreply.github.com>
2023-03-08 10:56:04 +0900

    feat(tokenize): tokenize가 끝난 token 내 빈 값이 있는지 확인하는 함수 추가

--- parse/parse.c
+++ parse/parse.c
@@ -12,6 +12,8 @@ char	**parse_command_line(char *line)
 		return ;
 	token = tokenize_line(trimmed_line);
 	free(trimmed_line);
+	if (has_empty_value_in_token(token))
+		// TODO => implements has_empty_value_in_token()
 	if (token && is_valid_syntax(token))
 		//TODO => implements is_valid_syntax()
 	else
