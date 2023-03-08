35e8817d1d5e24be7b54ca7c9c79877ac72fc81e
yena <50291995+nyj001012@users.noreply.github.com>
2023-03-08 11:40:15 +0900

    bug(tokenize): join_token_value() 호출 시, (문자열 + 문자열)을 (문자열 + 문자)가 되도록 수정

--- parse/token/join_token_value.c
+++ parse/token/join_token_value.c
@@ -4,19 +4,21 @@ void	join_token_value(t_token **token, char *trimmed_line, int *i)
 {
 	char	*new_value;
 	char	*temp;
+	char	c;
 
 	if (!token || !*token)
 		*token = create_new_token(NULL, 0);
 	else if (!trimmed_line || !trimmed_line[*i])
 		return ;
+	c = trimmed_line[*i];
 	if (!(*token)->value)
 	{
-		(*token)->value = ft_strdup(&trimmed_line[*i]);
+		(*token)->value = ft_strdup(&c);
 		return ;
 	}
 	else
 	{
-		new_value = ft_strjoin((*token)->value, &trimmed_line[*i]);
+		new_value = ft_strjoin((*token)->value, &c);
 		temp = (*token)->value;
 		(*token)->value = new_value;
 		free(temp);
