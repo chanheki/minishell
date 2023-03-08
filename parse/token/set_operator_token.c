e764122a1e309927456df5671c9d33bc616f96ec
yena <50291995+nyj001012@users.noreply.github.com>
2023-03-08 12:23:38 +0900

    bug(tokenize): double operator와 single operator 구분 안 되는 버그 수정

--- parse/token/set_operator_token.c
+++ parse/token/set_operator_token.c
@@ -36,27 +36,27 @@ enum e_token_type	get_double_operator_token_type(char c)
 
 void	set_operator_token(t_token **token, char *trimmed_line, int *i)
 {
-	char	operator;
+	char	*operator;
 
 	if ((*token)->value)
 		return ;
-	operator = trimmed_line[*i];
-	if (ft_strchr("();", operator))
+	operator = &trimmed_line[*i];
+	if (ft_strchr("();", *operator))
 	{
 		join_token_value(token, trimmed_line, i);
-		(*token)->type = get_operator_token_type(operator);
+		(*token)->type = get_operator_token_type(*operator);
 		(*i)++;
 	}
 	else if (ft_strchr("<>&|", trimmed_line[*i]))
 	{
-		while (trimmed_line[*i] == operator)
+		while (trimmed_line[*i] == *operator)
 		{
 			join_token_value(token, trimmed_line, i);
 			(*i)++;
 		}
-		if ((trimmed_line[*i] - operator) > 1)
-			(*token)->type = get_double_operator_token_type(operator);
+		if ((&trimmed_line[*i] - operator) == 2)
+			(*token)->type = get_double_operator_token_type(*operator);
 		else
-			(*token)->type = get_operator_token_type(operator);
+			(*token)->type = get_operator_token_type(*operator);
 	}
 }
