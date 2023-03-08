80c8e1f10bc33122c213bad3a611ab2769f525dc
yena <50291995+nyj001012@users.noreply.github.com>
2023-03-08 11:24:55 +0900

    update(tokenize): 불필요한 필드 할당 삭제 및 필드 명 변경

--- parse/token/create_new_token.c
+++ parse/token/create_new_token.c
@@ -1,4 +1,4 @@
-#include "libft.h"
+#include "../../include/parse.h"
 
 t_token	*create_new_token(void *value, enum e_token_type type)
 {
@@ -7,9 +7,8 @@ t_token	*create_new_token(void *value, enum e_token_type type)
 	new_token = (t_token *)malloc(sizeof(t_token));
 	new_token->value = value;
 	new_token->type = type;
-	new_token->is_in_quotes = false;
-	new_token->is_in_dquotes = false;
-	new_token->is_in_parentesis = false;
+	new_token->is_in_quote = false;
+	new_token->is_in_dquote = false;
 	new_token->is_in_escape = false;
 	new_token->next = NULL;
 	return (new_token);
