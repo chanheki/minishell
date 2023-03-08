4163f30faa9218719cccd6c1bf0071d3eee9cd42
yena <50291995+nyj001012@users.noreply.github.com>
2023-03-08 11:29:50 +0900

    update(tokenize): trimmed_line의 인덱싱 용으로 i에서 *i로 변경

--- parse/token/set_token.c
+++ parse/token/set_token.c
@@ -2,13 +2,13 @@
 
 void	set_token(t_token *token, char *trimmed_line, int *i)
 {
-	if (trimmed_line[i] == ESCAPE)
+	if (trimmed_line[*i] == ESCAPE)
 		set_normal_token(&token, trimmed_line, i);
-	else if (trimmed_line[i] == QUOTE || trimmed_line[i] == DQUOTE)
+	else if (trimmed_line[*i] == QUOTE || trimmed_line[*i] == DQUOTE)
 		set_quote_token(&token, trimmed_line, i);
-	else if (ft_strchr("<>()|;&", trimmed_line[i]))
+	else if (ft_strchr("<>()|;&", trimmed_line[*i]))
 		set_operator_token(&token, trimmed_line, i);
-	else if (ft_isdigit(trimmed_line[i]))
+	else if (ft_isdigit(trimmed_line[*i]))
 		set_fd_redirection_token(&token, trimmed_line, i);
 	else
 		set_normal_token(&token, trimmed_line, i);
