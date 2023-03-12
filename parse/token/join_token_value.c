4456b6a693ad2e8565cd9d8c4035c36dc39ee90f
yena <50291995+nyj001012@users.noreply.github.com>
2023-03-12 12:17:53 +0900

    bug(tokenize): 토큰 값 join 시, 의도치 않은 값이 같이 join되는 현상 수정

--- parse/token/join_token_value.c
+++ parse/token/join_token_value.c
@@ -1,26 +1,41 @@
 #include "../../include/parse.h"
 
+char	*get_join_source(char c)
+{
+	char	*source;
+
+	source = (char *)malloc(sizeof(char) * 2);
+	if (!source)
+		return (0);
+	source[0] = c;
+	source[1] = '\0';
+	return (source);
+}
+
 void	join_token_value(t_token **token, char *trimmed_line, int *i)
 {
 	char	*new_value;
 	char	*temp;
-	char	c;
+	char	*source;
 
 	if (!token || !*token)
 		*token = create_new_token(NULL, 0);
 	else if (!trimmed_line || !trimmed_line[*i])
 		return ;
-	c = trimmed_line[*i];
+	source = get_join_source(trimmed_line[*i]);
+	if (!source)
+		return ;
 	if (!(*token)->value)
 	{
-		(*token)->value = ft_strdup(&c);
+		(*token)->value = source;
 		return ;
 	}
 	else
 	{
-		new_value = ft_strjoin((*token)->value, &c);
+		new_value = ft_strjoin((*token)->value, source);
 		temp = (*token)->value;
 		(*token)->value = new_value;
 		free(temp);
+		free(source);
 	}
 }
