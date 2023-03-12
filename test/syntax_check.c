66d046384b9a981b16243d9cdc9eb7760a0b7060
yena <50291995+nyj001012@users.noreply.github.com>
2023-03-12 11:59:34 +0900

    test(syntax): 토큰을 이용한 문법 체크 테스트 추가

---
+++ test/syntax_check.c
@@ -0,0 +1,30 @@
+#include "../include/parse.h"
+
+int	main(void)
+{
+	char		*input;
+	char		*trimmed;
+	int 		read_bytes;
+	t_token		*token;
+
+	input = (char *)malloc(sizeof(char) * (BUFSIZ + 1));
+	while (1)
+	{
+		ft_putstr_fd("type command line => ", STDOUT_FILENO);
+		read_bytes = read(STDIN_FILENO, input, BUFSIZ);
+		if (read_bytes < 1)
+			break ;
+		input[read_bytes] = '\0';
+		trimmed = ft_strtrim(input, " \n");
+		token = tokenize_line(trimmed);
+		free(trimmed);
+		free_token_list(&token);
+		if (is_valid_syntax(token))
+			ft_putstr_fd("OK\n", STDOUT_FILENO);
+		else
+			ft_putstr_fd("KO\n", STDERR_FILENO);
+		ft_memset(input, 0, read_bytes);
+	}
+	free(input);
+	return (0);
+}
