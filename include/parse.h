de0be21c92b8148d6cc5fa98f5a6079b77011cc1
yena <50291995+nyj001012@users.noreply.github.com>
2023-03-12 16:42:50 +0900

    update(token): token의 우선순위 별 정렬

--- include/parse.h
+++ include/parse.h
@@ -7,17 +7,17 @@
 
 enum	e_token_type {
 	NORMAL,
-	DREDIRECT_IN,
 	REDIRECT_IN = '<',
-	DREDIRECT_OUT,
+	DREDIRECT_IN,
 	REDIRECT_OUT = '>',
+	DREDIRECT_OUT,
 	PARENTHESIS_OPEN = '(',
 	PARENTHESIS_CLOSE = ')',
-	PIPE = '|',
-	SEMICOLON = ';',
-	DAMPERSAND,
-	DPIPE,
 	AMPERSAND = '&',
+	DAMPERSAND,
+	PIPE = '|',
+	DPIPE,
+	SEMICOLON = ';',
 	QUOTE = '\'',
 	DQUOTE = '\"',
 	WILDCARD = '*',
@@ -90,6 +90,7 @@ t_ASTnode			*parse_command_line(char *line);
 bool				is_valid_syntax(t_token *token);
 bool				is_valid_redirection(t_token *token, char **token_value);
 bool				is_pair_of_parenthesis(t_token *token, char **token_value);
+t_token				*get_last_token_in_parenthesis(t_token **token);
 bool				is_valid_parenthesis(t_token *token, char **token_value);
 bool				is_valid_command(t_token *token, char **token_value);
 t_ASTnode			*make_ast_tree(t_token **token);
@@ -103,7 +104,7 @@ t_ASTnode			*set_ast_node(char *trimmed_line);
 t_ASTnode			*get_parent_node(t_ASTnode *node, t_token *current);
 t_ASTnode			*get_root_node(t_ASTnode *ast_tree);
 int					make_parenthesis_node(t_ASTnode **ast_tree, t_token **current);
-int					make_operator_node(t_ASTnode **ast_tree, t_token **current);
+int					make_operator_node(t_ASTnode **ast_tree, t_token *current);
 int					make_command_node(t_ASTnode **ast_tree, t_token **current);
 int 				make_redirection_node(t_ASTnode **ast_tree, t_token **current);
 int 				make_normal_node(t_ASTnode **ast_tree, t_token **current);
@@ -128,6 +129,7 @@ void				set_fd_redirection_token(t_token **token,
 void				set_token(t_token *token, char *trimmed_line, int *i);
 void				free_token_list(t_token **token);
 t_token				*tokenize_line(char *trimmed_line);
+bool				is_operator(t_token *token);
 
 /*------------------------------ COMMAND_SPLIT -------------------------------*/
 size_t				get_split_command_count(char *arguments);
