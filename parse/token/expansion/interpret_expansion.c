4d0d21cc045466c7e6c8d751f1ba52f6b5405265
yena <50291995+nyj001012@users.noreply.github.com>
2023-03-26 15:39:44 +0900

    refactor(token): expansion 모듈은 token/expansion으로 이동

--- parse/token/expansion/interpret_expansion.c
+++ parse/token/expansion/interpret_expansion.c
@@ -1,29 +1,4 @@
-#include "../../include/parse.h"
-
-/*
- * Description: 이전 foreground에서 실행된 명령어의 exit status를 토큰의 value에 저장한다.
- * Param.   #1: 토큰의 주소
- * Param.   #2: 해석할 환경변수가 포함된 커맨드 라인
- * Param.   #3: 커맨드 라인 내 환경변수의 시작 인덱스. '?'의 위치를 가리키고 있음
- * Return     : 없음
- */
-void	interpret_exit_status(t_token **token, int *i)
-{
-	char	*exit_status;
-	char	*temp;
-
-	exit_status = ft_itoa(0);
-	if (!(*token)->value)
-		(*token)->value = exit_status;
-	else
-	{
-		temp = (*token)->value;
-		(*token)->value = ft_strjoin((*token)->value, exit_status);
-		free(temp);
-		free(exit_status);
-	}
-	*i += 2;
-}
+#include "../../../include/parse.h"
 
 /*
  * Description: 환경변수 키를 찾아서 그 값을 반환한다.
