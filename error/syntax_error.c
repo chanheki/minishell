59adf9241de70126ba591ce3bfe8ff5e25af00ec
Yejin Na <yena@c10r3s12.42seoul.kr>
2023-05-04 18:57:21 +0900

    feat(syntax): 파싱에서 에러를 출력하는 모듈 추가

---
+++ error/syntax_error.c
@@ -0,0 +1,24 @@
+#include "../include/minishell.h"
+
+/*
+ * Description: 토큰의 syntax error를 출력한다.
+ * Param.   #1: 에러가 발생한 토큰의 주소
+ * Return     : 없음
+ */
+void    print_syntax_error(t_token *token)
+{
+    ft_putstr_fd("JIP-shell: syntax error near unexpected token `", 2);
+    ft_putstr_fd(token->value, 2);
+    ft_putendl_fd("'", 2);
+}
+
+/*
+ * Description: 토큰의 command not found를 출력한다.
+ * Param.   #1: 에러가 발생한 토큰의 주소
+ * Return     : 없음
+ */
+ void   print_command_not_found(t_token *token)
+ {
+    ft_putstr_fd("JIP-shell: command not found: ", 2);
+	ft_putendl_fd(token->value, 2);
+ }
