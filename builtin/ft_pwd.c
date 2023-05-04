58da0f38d0f20089dfb8b9ca18d213b855496560
chanhihi <chanhihi55@gmail.com>
2023-05-04 22:14:55 +0900

    refact(pwd): builtin

--- builtin/ft_pwd.c
+++ builtin/ft_pwd.c
@@ -5,7 +5,16 @@
 
 #include "../include/minishell.h"
 
-int	ft_pwd()
+int	ft_pwd(char **argv)
 {
+	char	*path;
+
+	argv = (char **)argv;
+	path = getcwd(NULL, 0);
+	if (!path)
+		return (1);
+	ft_putendl_fd(path, STDOUT_FILENO);
+	free(path);
+	path = NULL;
 	return (0);
 }
