cab7c19d7d043f8e4eb78d2278c2d8b79e2b6d2f
Yejin Na <yena@c10r3s12.42seoul.kr>
2023-05-08 15:02:08 +0900

    feat(builtin): implement pwd

--- builtin/ft_pwd.c
+++ builtin/ft_pwd.c
@@ -1,11 +1,28 @@
-
-// pwd with no options
-
-// 　옵션이 없는 pwd
-
 #include "../include/minishell.h"
 
-int	ft_pwd()
+/*
+ * Description: 현재 디렉토리 경로를 출력한다.
+ * Param.     : 없음
+ * Return     : 성공하면 0, 실패하면 1을 반환한다.
+ */
+int	ft_pwd(void)
 {
-	return (0);
+	size_t	size;
+	char	*working_dir;
+
+	size = 1024;
+	working_dir = (char *)malloc(sizeof(char) * (size + 1));
+	working_dir[size] = '\0';
+	if (getcwd(working_dir, size))
+	{
+		ft_putstr_fd(working_dir, STDOUT_FILENO);
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		return (0);
+	}
+	else
+	{
+		ft_putstr_fd("minishell: pwd: can't find current working directory\n",
+					STDERR_FILENO);
+		return (1);
+	}
 }
