1a0ca440631bac659e03eef01c615cae675c21f8
yena <50291995+nyj001012@users.noreply.github.com>
2023-05-09 19:21:56 +0900

    feat(env): add dictionary type of env

--- include/env.h
+++ include/env.h
@@ -3,6 +3,20 @@
 
 # include "minishell.h"
 
+typedef struct	s_env_dict
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+	struct s_env	*prev;
+}	t_env_dict;
+
+typedef struct	s_env
+{
+	t_env_dict		*dict;
+	char			**env_str;
+}	t_env;
+
 /*----------------------------------- ENV -----------------------------------*/
 int	initialize_envp(char **env);
 
