0f2cff1c0ce8a54e7f85458ab46bf65f1da210e7
yena <50291995+nyj001012@users.noreply.github.com>
2023-05-09 20:27:25 +0900

    chore(env): add description

--- env/dict/free.c
+++ env/dict/free.c
@@ -1,5 +1,10 @@
 #include "../../include/minishell.h"
 
+/*
+ * Description: 환경 변수 딕셔너리를 할당 해제한다.
+ * Param.   #1: 환경 변수 딕셔너리
+ * Return     : 없음
+ */
 void	free_env_dict(t_env_dict *dict)
 {
 	t_env_dict	*tmp;
