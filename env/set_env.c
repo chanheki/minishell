4f91ef6622eaade2c34ae6e2edc774ff612f1c69
yena <50291995+nyj001012@users.noreply.github.com>
2023-05-09 23:49:28 +0900

    feat(builtin): implement change_value

--- env/set_env.c
+++ env/set_env.c
@@ -1,6 +1,33 @@
 #include "../include/minishell.h"
 
 /*
+ * Description: key에 맞는 환경변수의 value를 변경한다.
+ * Param.   #1: 변경할 환경변수의 키
+ * Param.   #2: 변경할 환경변수의 값
+ * Return     : SUCCESS: 성공
+ *            : ERROR  : 실패
+ */
+int	change_value(char *key, char *value)
+{
+	t_env_dict	*temp;
+
+	temp = g_var.env_dict;
+	while (temp)
+	{
+		if (!ft_strcmp(temp->key, key))
+		{
+			free(temp->value);
+			temp->value = ft_strdup(value);
+			if (!temp->value)
+				return (ERROR);
+			return (SUCCESS);
+		}
+		temp = temp->next;
+	}
+	return (ERROR);
+}
+
+/*
  * Description: 환경변수를 추가한다.
  * Param.   #1: 추가할 환경변수의 키
  * Param.   #2: 추가할 환경변수의 값
