0e28d38602aef8050b40cb9fe4a8dccdbde74b63
yena <50291995+nyj001012@users.noreply.github.com>
2023-05-10 00:21:35 +0900

    feat(builtin): implement env

--- builtin/ft_env.c
+++ builtin/ft_env.c
@@ -1,11 +1,19 @@
-
-// env with no options or arguments
-
-// 　옵션이나 인자값이 없는 env
-
 #include "../include/minishell.h"
 
-int	ft_env()
+/*
+ * Description: 환경변수를 출력한다.
+ * Param.     : 없음
+ * Return     : 없음
+ */
+int	ft_env(void)
 {
+	int	i;
+
+	i = -1;
+	while (g_var.envp[++i])
+	{
+		ft_putstr_fd(g_var.envp[i], STDOUT_FILENO);
+		ft_putstr_fd("\n", STDOUT_FILENO);
+	}
 	return (0);
 }
