94e15ce62fff2ef6abd64ecd85d4d48e4ac9e325
yena <50291995+nyj001012@users.noreply.github.com>
2023-05-10 14:49:10 +0900

    feat(builtin): implement set_env OLDPWD also

--- builtin/ft_cd.c
+++ builtin/ft_cd.c
@@ -33,7 +33,7 @@ int	move_to_dir(char *path)
 }
 
 /*
- * Description: 환경 변수에 저장된 현재 작업 디렉토리를 변경한다.
+ * Description: 환경 변수에 저장된 현재 작업 디렉토리와 이전 디렉토리를 변경한다.
  * Param.  #1 : 변경할 디렉토리 경로
  * Return     : 성공 시 0, 실패 시 1
  */
@@ -47,9 +47,18 @@ int	update_cwd(char *path)
 		ft_putstr_fd("minishell: cd: getcwd failed\n", 2);
 		return (1);
 	}
-	delete_env("PWD");
-	set_env("PWD", path);
+	if (set_env("OLDPWD", cwd) == ERROR)
+	{
+		ft_putstr_fd("minishell: cd: OLDPWD setenv failed\n", 2);
+		free (cwd);
+		return (1);
+	}
 	free(cwd);
+	if (set_env("PWD", path) == ERROR)
+	{
+		ft_putstr_fd("minishell: cd: PWD setenv failed\n", 2);
+		return (1);
+	}
 	return (0);
 }
 
