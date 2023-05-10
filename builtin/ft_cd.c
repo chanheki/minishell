3d25d23425208a27e82071926f63300c7201e9a6
yena <50291995+nyj001012@users.noreply.github.com>
2023-05-10 14:15:36 +0900

    feat(builtin): implement move_to_home, update_cwd

--- builtin/ft_cd.c
+++ builtin/ft_cd.c
@@ -1,6 +1,50 @@
 #include "../include/minishell.h"
 
 /*
+ * Description: 환경 변수에 저장된 현재 작업 디렉토리를 변경한다.
+ * Param.  #1 : 변경할 디렉토리 경로
+ * Return     : 성공 시 0, 실패 시 1
+ */
+int	update_cwd(char *path)
+{
+	char	*cwd;
+
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+	{
+		ft_putstr_fd("minishell: cd: getcwd failed\n", 2);
+		return (1);
+	}
+	delete_env("PWD");
+	set_env("PWD", path);
+	free(cwd);
+	return (0);
+}
+
+/*
+ * Description: 환경 변수에 저장된 HOME으로 현재 작업 디렉토리를 변경한다.
+ * Param.  #1 : 없음
+ * Return     : 성공 시 0, 실패 시 1
+ */
+int	move_to_home(void)
+{
+	char	*home_path;
+
+	home_path = find_value("HOME");
+	if (!home_path)
+	{
+		ft_putstr_fd("minishell: cd: HOME not set\n", 2);
+		return (1);
+	}
+	if (!chdir(home_path))
+	{
+		ft_putstr_fd("minishell: cd: change directory to HOME failed\n", 2);
+		return (1);
+	}
+	return (update_cwd(home_path));
+}
+
+/*
  * Description: *path 값에 따라 현재 작업 디렉토리를 변경한다.
  * Param.  #1 : 변경할 디렉토리 경로
  * Return     : 성공 시 0, 실패 시 1
