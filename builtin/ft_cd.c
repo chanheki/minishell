d444e45557cefd20c9792c8a947bdd6d7cba33d3
yena <50291995+nyj001012@users.noreply.github.com>
2023-05-10 14:43:43 +0900

    feat(builtin): implement move_to_prev_dir

--- builtin/ft_cd.c
+++ builtin/ft_cd.c
@@ -77,6 +77,29 @@ int	move_to_home(void)
 }
 
 /*
+ * Description: 환경 변수에 저장된 OLDPWD로 현재 작업 디렉토리를 변경한다.
+ * Param.  #1 : 없음
+ * Return     : 성공 시 0, 실패 시 1
+ */
+int	move_to_prev_dir(void)
+{
+	char	*oldpwd;
+
+	oldpwd = find_value("OLDPWD");
+	if (!oldpwd)
+	{
+		ft_putstr_fd("minishell: cd: OLDPWD not set\n", 2);
+		return (1);
+	}
+	if (!chdir(oldpwd))
+	{
+		ft_putstr_fd("minishell: cd: change directory to OLDPWD failed\n", 2);
+		return (1);
+	}
+	return (update_cwd(oldpwd));
+}
+
+/*
  * Description: *path 값에 따라 현재 작업 디렉토리를 변경한다.
  * Param.  #1 : 변경할 디렉토리 경로
  * Return     : 성공 시 0, 실패 시 1
