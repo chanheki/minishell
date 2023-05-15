967c1f8112778cde642f5439ae81b247426bc6ae
chanhihi <chanhihi55@gmail.com>
2023-05-15 11:11:24 +0900

    feat(env): env argv hotfix

--- builtin/ft_env.c
+++ builtin/ft_env.c
@@ -17,11 +17,18 @@
  * Param.     : 없음
  * Return     : 없음
  */
-int	ft_env(void)
+int	ft_env(char **argv)
 {
 	int	i;
 
 	i = -1;
+	if (argv[1])
+	{
+		ft_putstr_fd("env: ", STDERR_FILENO);
+		ft_putstr_fd(argv[1], STDERR_FILENO);
+		ft_putendl_fd(": No such file or directory", STDERR_FILENO);
+		return (127);
+	}
 	while (g_var.envp[++i])
 	{
 		ft_putstr_fd(g_var.envp[i], STDOUT_FILENO);
